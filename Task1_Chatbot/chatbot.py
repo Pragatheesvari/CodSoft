0e8f037f821d64f73917a38900a3a18b1c025f00
Pragatheesvari <Jveswari025@gmail.com>
2026-06-23 21:17:03 +0530

    Add response for AI usage inquiries in chatbot

--- Task1_Chatbot/chatbot.py
+++ Task1_Chatbot/chatbot.py
@@ -7,8 +7,12 @@ while True:
         print("Bot: Hello! How are you?")
     elif "your name" in user:
         print("Bot: I am a simple AI Chatbot.")
+    elif "uses of ai" in user:
+        print("Bot: AI is used in healthcare, education, chatbots, self-driving cars, recommendation systems, fraud detection, virtual assistants, and many other fields.")
     elif "ai" in user:
         print("Bot: AI stands for Artificial Intelligence.")
+    elif "ml" in user:
+        print("Bot: Machine Learning is a subset of Artifical Intelligence.") 
     elif "how are you" in user:
         print("Bot: I am doing great!")
     elif user == "bye":
